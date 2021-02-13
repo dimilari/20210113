1ebbdce5bb94fa8d7cde31a6d321aaa54616eb1b
Dimitri Larios <lariosdimitri@gmail.com>
2021-02-13 10:52:44 +0100

    initial commit

---
+++ Teclado.py
@@ -0,0 +1,36 @@
+import keyboard
+import time
+# keyboard.wait('a')
+# keyboard.write('á')
+
+# a = keyboard.read_hotkey()
+# b = keyboard.read_key()
+
+# keyboard.start_recording()
+# keyboard.wait('enter')
+# keyboard.stop_recording()
+# keyboard.record(until='enter')
+
+# a = keyboard.record(until='enter')
+# keyboard.play(a)
+
+# keyboard.add_word_listener('aa', func, 'a')
+# keyboard.wait('enter')
+
+# a = keyboard.record(until='enter')
+# print(a)
+# keyboard.press('backspace down')
+# KeyboardEvent(backspace down), KeyboardEvent(backspace up)
+
+
+########## keyboard.press(('backspace', 'backspace','backspace'))
+
+# a = keyboard.read_key()
+# print(a)
+
+keyboard.start_recording()
+keyboard.wait('a')
+a = keyboard.stop_recording()
+b = list(map(str, a))
+print(type(b[0]))
+
