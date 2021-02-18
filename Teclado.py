8c8fd22e50b9fa70efbd4a2458636d65fc252873
Dimitri Larios <lariosdimitri@gmail.com>
2021-02-18 20:59:53 +0100

    bad worked commit

--- Teclado.py
+++ Teclado.py
@@ -16,23 +16,25 @@ def language():
     return StartLang
 
 a = []
-chars = 'aeioun?!AEIOUN'
-spanish_chars = 'áéíóúñ¿¡ÁÉÍÓÚÑ'
+chars = 'aeioun?!AEIOUN`'
+spanish_chars = 'áéíóúñ¿¡ÁÉÍÓÚÑ`'
 while True:
     if language() == 67699721:
         win32api.LoadKeyboardLayout("00000409", 1)
-        if len(pushed_key()) > 1:
-            continue
-        a.append(pushed_key())
-        print(a, language())
-        if a[-1] == '`' and a[0] in chars:
-            ind = chars.find(a[0])
-            keyboard.press('backspace')
-            # pausa, usando la librería 'time'
-            sleep(0.01)
-            keyboard.press('backspace')
-            keyboard.write(spanish_chars[ind])
-        if len(a) >= 2:
-            a = a[1:]
+        # if len(pushed_key()) > 1:
+        #     continue
+        if pushed_key() in chars and len(pushed_key()) < 2:
+            a.append(pushed_key())
+            print(a, language())
+            if a[-1] == '`' and a[0] in chars:
+                ind = chars.find(a[0])
+                keyboard.press('backspace')
+                # pausa, usando la librería 'time'
+                sleep(0.01)
+                keyboard.press('backspace')
+                keyboard.write(spanish_chars[ind])
+            if len(a) >= 2:
+                a = a[1:]
+                print(a)
 
 
